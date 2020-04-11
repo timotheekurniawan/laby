2ff2ef71ea75fb46eb35c51ed3315a8ac803d310
michaelwanghku <michwang@connect.hku.hk>
2020-04-11 23:57:06 +0700

    Updated levels.cpp

--- src/levels.cpp
+++ src/levels.cpp
@@ -1,15 +1,133 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 #include "levels.h"
 using namespace std;
 
-// belom tau ini gmn cara define function nya selalu ada error "this declaration has no storage class or type specifier"
+//DEFINE HEARTS
 
-// string levels[10] = {
-//   "----------------------------------------------------------------------------------------------------"
-//   "----------------------------------------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-// };
+string maps[10] = {
+  "----------------------------------------------------------------------------------------------------"
+  "----------------------------------------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+};
 
-// string getEmptyMap(int level){
-//   return;
-// }
+// Move limit of each level
+int limits[10] = {100, 200};
+
+// Spaces to be traveled by laby of each level
+// kalo dah traveledCount == 67 (misalnya), artinya covered semua
+int spaces[10] = {67, 45};
+
+// Function to get the empty map of the level
+// Input    : - level
+// Return   : - the empty map
+string getEmptyMap(int level){
+  return maps[level-1];
+}
+
+// Function to get the initial limit of the level
+// Input    : - level
+// Return   : - the initial limit
+int getInitialLimit(int level){
+  return limits[level-1];
+}
+
+void randomizeApplePos(string &map){
+  srand(time(NULL));
+  int applePos = rand() % 100;
+  map.replace(applePos, 1, HEART);
+  return;
+}
+
+void buildMap(string map){
+  randomizeApplePos(map);
+
+}
+
+int main(){
+  for (int i = 0; i < 10; ++i){
+    cout << " -";
+  }
+  cout << endl;
+  for (int j = 0; j < 11; ++j){
+    cout << "| ";
+  }
+  cout << endl;
+  for (int i = 0; i < 10; ++i){
+    cout << " -";
+  }
+  cout << endl;
+  for (int j = 0; j < 11; ++j){
+    cout << "| ";
+  }
+  cout << endl;
+  for (int i = 0; i < 10; ++i){
+    cout << " -";
+  }
+  cout << endl;
+  for (int j = 0; j < 11; ++j){
+    cout << "| ";
+  }
+  cout << endl;
+  for (int i = 0; i < 10; ++i){
+    cout << " -";
+  }
+  cout << endl;
+  for (int j = 0; j < 11; ++j){
+    cout << "| ";
+  }
+  cout << endl;
+  for (int i = 0; i < 10; ++i){
+    cout << " -";
+  }
+  cout << endl;
+  for (int j = 0; j < 11; ++j){
+    cout << "| ";
+  }
+  cout << endl;
+  for (int i = 0; i < 10; ++i){
+    cout << " -";
+  }
+  cout << endl;
+  for (int j = 0; j < 11; ++j){
+    cout << "| ";
+  }
+  cout << endl;
+  for (int i = 0; i < 10; ++i){
+    cout << " -";
+  }
+  cout << endl;
+  for (int j = 0; j < 11; ++j){
+    cout << "| ";
+  }
+  cout << endl;
+  for (int i = 0; i < 10; ++i){
+    cout << " -";
+  }
+  cout << endl;
+  for (int j = 0; j < 11; ++j){
+    cout << "| ";
+  }
+  cout << endl;
+  for (int i = 0; i < 10; ++i){
+    cout << " -";
+  }
+  cout << endl;
+  for (int j = 0; j < 11; ++j){
+    cout << "| ";
+  }
+  cout << endl;
+  for (int i = 0; i < 10; ++i){
+    cout << " -";
+  }
+  cout << endl;
+  for (int j = 0; j < 11; ++j){
+    cout << "| ";
+  }
+  cout << endl;
+  for (int i = 0; i < 10; ++i){
+    cout << " -";
+  }
+  cout << endl;
+}
--- src/levels.h
+++ src/levels.h
@@ -1,9 +1,11 @@
 //currentLevels.h
 //contain all 10 currentLevels, function to get empty maps of each currentLevel, and get initial moves of each currentLevel
+#include <string>
 #ifndef currentLevelS_H
 #define currentLevelS_H
 
-void getEmptyMap(int level);
-int getInitialMoves(int level);
+std::string getEmptyMap(int level);
+int getInitialLimit(int level);
+void buildMap(std::string map);
 
 #endif
