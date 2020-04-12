83e5d036e0dac7008c076c0475f521b941dd62ea
michaelwanghku <michwang@connect.hku.hk>
2020-04-12 12:02:04 +0700

    Updated fout values

--- src/congrats.cpp
+++ src/congrats.cpp
@@ -7,12 +7,12 @@
 
 using namespace std;
 
-//Function used when a user has finished all 10 levels
-//Function displays congratulation message and asks the user wether to restart the game or quit the game
-//If the user restarts game, it resets user's game progress(currentLevel,currentMap,etc.) to level 1
-//PlayGame(username) to play the game again
-//If the user quits game, the user's game progress(currentLevel,currentMap,etc.) is stored in username.txt
-void CompletedGame(User &userSelected){
+// Function used when a user has finished all 10 levels.
+// It displays congratulation message and asks the user whether to restart the game or quit the game.
+// - If the user restarts game, it resets user's game progress(currentLevel, currentMap, and currentLimit) to level 1
+//   PlayGame(username) to play the game again.
+// - If the user quits game, the user's game progress(currentLevel,currentMap,etc.) is stored in username.txt.
+void completedGame(User &userSelected){
     string filename=userSelected.username+".txt";
     ofstream fout;
     fout.open(filename);
@@ -30,14 +30,14 @@ void CompletedGame(User &userSelected){
         userSelected.currentLimit = getInitialLimit(userSelected.currentLevel);
         // username.heartPos[0] = randomizeHeartPos;
         // username.heartPos[1] = randomizeHeartPos;
-        //randomizeHeartPos(username.currentMap);
-        userSelected.heartExist = 1;
+        // randomizeHeartPos(username.currentMap);
+        //userSelected.heartExist = 1;
         PlayGame(userSelected);
     }
     else if (selection==2)
     {
         userSelected.currentLevel = 11;
-        fout<<userSelected.username<<userSelected.currentLevel<<userSelected.currentMap<<userSelected.currentLimit<<userSelected.heartPos[0]<<userSelected.heartPos[1]<<userSelected.heartExist;
+        fout<<userSelected.username<<userSelected.currentLevel<<userSelected.currentMap<<userSelected.currentLimit<<userSelected.currentTravel;
         fout.close();
     }
 }
