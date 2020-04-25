97ab4127a749f25833d6bef5ab4774020979b58f
michaelwanghku <michwang@connect.hku.hk>
2020-04-25 13:40:17 +0700

    Updated welcome.h

--- src/welcome.h
+++ src/welcome.h
@@ -7,7 +7,7 @@
 void addToUsernameList(std::string username);
 void displayExistingUsers();
 void getUser(std::string chosenUsername, User &chosenUser);
-void createUser(std::string newUsername, User &newUser)
+void createUser(std::string newUsername, User &newUser);
 User newGame();
 User continueGame();
 User welcome();
