25616a12455e8300edcdbf963b946b79abf40b7a
timotheekurniawan <timothee@connect.hku.hk>
2020-05-03 18:31:12 +0700

    Changed a level on the game

--- src/levels.cpp
+++ src/levels.cpp
@@ -14,13 +14,13 @@ using namespace std;
 
 // Map of each level in form of string
 string maps[10] = {
+"--XXXXXXXX----XXXXXX------XXXX--------XX-------------------X-------XXX-----XXXXX---XXXXXXX-XXXXXXXXX",
 "XXXXXXXXXX---------X-X-XX-X----X---------X-XX-X-X-----X-X-X-------X-X--X---XX-X--XX-----X-XXXXXXXXXX",
 "---XXXX----X-XXXX-X---------X-XX-XX---X----XX-XXX--X-XX------X-XXXXX---X-----X---XXXXX-X---------X--",
 "XX----XXXXXX-X----XXXX------XXXXXX--XXXXXXXX--XXXX--XX--XX-----X--X---X--------XXX---X--XXXXXX--XXXX",
 "X---X----X--------------X-X-X-----------------X----X-------X------------X-------X-------XXXXX----XXX",
 "XXXX--XXXXXXXX--XXXXXX------XXX--------XX---X-----X---X-X---------X-----X-----------------XX-----X--",
 "X---X-------X---XX-----------X--X----X--X-X--X---------XXX-X-XXX-X------------X---XX-XX-----XX-----X",
-"-----XXXXX-XX---XX---X----X-------------XX------X-XXXXX-X---------XXX--XXXX-X---------X---XXXXXXXXXX",
 "X--------X----XX------XXXXXX---XX----XX---X-XX-X--X--------XX---XX---XXX------XX---XXXX-------XX----",
 "-----XXXXX-XX---XX---X----X-------------XX------X-XXXXX-X---------XXX--XXXX-XXX--XXXX-X---------X---",
 "--X-------X-----X-X-XX----X-X-XXX-----X-XXXXXXXXX-XXXXXXXXX-XXX-----X-XX----X-X-X-----X-X---X-------"
@@ -30,7 +30,7 @@ string maps[10] = {
 int limits[10] = {500, 500, 500, 500, 500, 500, 500, 500, 500, 500};
 
 // Spaces to be traveled by laby of each level
-int spaces[10] = {57, 61, 47, 81, 68, 73, 61, 66, 63, 58};
+int spaces[10] = {55,57, 61, 47, 81, 68, 73, 66, 63, 58};
 
 // Function to get the initial limit of the level
 // Input    : - level
